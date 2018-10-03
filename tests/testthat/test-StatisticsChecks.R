7c3987d8e500fbdc00a15718519e6afef4d911f7
Curtis Miller <cmiller@math.utah.edu>
2018-10-03 14:44:13 -0600

    Changed and added tests for pval

--- tests/testthat/test-StatisticsChecks.R
+++ tests/testthat/test-StatisticsChecks.R
@@ -11,9 +11,22 @@ library(MCHT)
 context("Check statistical functions")
 
 test_that("pval works properly", {
-  expect_equal(pval(5, 1:10), 0.5)
+  set.seed(123)
+  expect_equal(pval(5, 1:10), 0.6)
   expect_equal(pval(5, 1:10, alternative = "less"), 0.5)
-  expect_equal(pval(6, 1:10, alternative = "greater"), 0.4)
-  expect_equal(pval(6, 1:10, alternative = "two.sided"), 0.8)
+  expect_equal(pval(6, 1:10, alternative = "greater"), 0.3)
+  expect_equal(pval(6, 1:10, alternative = "two.sided"), 0.6)
   expect_error(pval(6, 1:10, alternative = "none"))
+  expect_equal(pval(5, c(1:10, 5, 5, 5),
+                    unif_gen = MCHT:::gen_memo_rng(runif)),
+               0.769230769230769)
+  expect_equal(pval(5, c(1:10, 5, 5, 5),
+                    unif_gen = MCHT:::gen_memo_rng(runif, 123)),
+               0.692307692307692)
+  expect_equal(pval(5, c(1:10, 5, 5, 5),
+                    unif_gen = MCHT:::gen_memo_rng(runif, 123)),
+               0.692307692307692)
+  expect_equal(pval(5, c(1:10, 5, 5, 5),
+                    unif_gen = MCHT:::gen_memo_rng(runif)),
+               0.846153846153846)
 })
